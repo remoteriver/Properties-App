3cd1de598849c495fd2bda96d0a56020e2879d9b
remoteriver <remoteriver@gmail.com>
2021-07-13 09:34:26 +1000

    Add not found page

--- src/app/containers/Header/index.tsx
+++ src/app/containers/Header/index.tsx
@@ -13,9 +13,6 @@ const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
   },
@@ -27,18 +24,9 @@ export const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" className={classes.title}>
           Properties
         </Typography>
-        <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
   );
--- src/app/containers/NotFoundPage/index.tsx
+++ src/app/containers/NotFoundPage/index.tsx
@@ -1,5 +1,15 @@
+import { Box, Container, Typography } from '@material-ui/core';
 import React from 'react';
 
 export const NotFoundPage = () => {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Box m={30}></Box>
+        <Box textAlign="center">
+          <Typography variant="h2">404 Not Found</Typography>
+        </Box>
+      </Container>
+    </>
+  );
 };
